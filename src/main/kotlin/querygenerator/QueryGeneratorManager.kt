c2da9003f2ac8431dd8dc1d11cb8326304088726
goodgoodman <gjwnsan9420@gmail.com>
2021-06-04 13:14:54 +0900

    Remove unused files

--- src/main/kotlin/querygenerator/QueryGeneratorManager.kt
+++ src/main/kotlin/querygenerator/QueryGeneratorManager.kt
@@ -1,17 +1,15 @@
 package com.goodgoodman.otter.querygenerator
 
-import com.goodgoodman.otter.querygenerator.exception.NotSupportedAdapterException
-
 object QueryGeneratorManager {
     fun getQueryGeneratorByDriverClassName(driverClassName: String): QueryGenerator {
         return when (driverClassName) {
             "org.h2.Driver" ->
-                H2QueryGenerator()
+                object : QueryGenerator() {}
             "com.mysql.cj.jdbc.Driver",
             "org.mariadb.jdbc.Driver" ->
-                MySqlQueryGenerator()
+                object : QueryGenerator() {}
             else ->
-                throw NotSupportedAdapterException(driverClassName)
+                object : QueryGenerator() {}
         }
     }
 }
--- src/main/kotlin/querygenerator/exception/NotSupportedAdapterException.kt
+++
@@ -1,3 +0,0 @@
-package com.goodgoodman.otter.querygenerator.exception
-
-class NotSupportedAdapterException(driverClassName: String) : Exception("[$driverClassName] is not supported.")
