b7afaccf2030d6edf870bf56925cca005a430c74
goodgoodman <gjwnsan9420@gmail.com>
2021-09-29 15:48:46 +0900

    Spring Boot 시작시 JPA-EntityManager 가 DDL 처리 하기 전에 Otter가 콜될 수 있도록 수정

--- otter-spring-boot-starter/src/main/kotlin/OtterAutoConfiguration.kt
+++ otter-spring-boot-starter/src/main/kotlin/OtterAutoConfiguration.kt
@@ -1,36 +1,40 @@
 package io.github.goodgoodjm.otter
 
-import io.github.goodgoodjm.otter.core.Logger
 import io.github.goodgoodjm.otter.core.OtterConfig
+import org.springframework.boot.autoconfigure.AutoConfigureAfter
+import org.springframework.boot.autoconfigure.condition.*
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.sql.init.dependency.DatabaseInitializationDependencyConfigurer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 
-@Configuration
-@EnableConfigurationProperties(OtterProperties::class)
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty(prefix = "otter", name = ["enable"], matchIfMissing = true)
+@AutoConfigureAfter(DataSourceAutoConfiguration::class, HibernateJpaAutoConfiguration::class)
+@Import(DatabaseInitializationDependencyConfigurer::class)
 open class OtterAutoConfiguration(
-    properties: OtterProperties,
 ) {
-    companion object : Logger
+    @Configuration(proxyBeanMethods = false)
+    @EnableConfigurationProperties(OtterProperties::class)
+    open class OtterConfiguration(
+        properties: OtterProperties,
+    ) {
+        private val config: OtterConfig = OtterConfig(
+            migrationPath = properties.migrationPath,
+            driverClassName = properties.driverClassName,
+            url = properties.url,
+            user = properties.username,
+            password = properties.password,
+            showSql = properties.showSql,
+        )
 
-    private val enabled: Boolean = properties.enabled
+        @Bean
+        open fun config(): OtterConfig = config
 
-    private val config: OtterConfig = OtterConfig(
-        migrationPath = properties.migrationPath,
-        driverClassName = properties.driverClassName,
-        url = properties.url,
-        user = properties.username,
-        password = properties.password,
-        showSql = properties.showSql,
-    )
-
-    init {
-        logger.info("Configure otter by $properties")
+        @Bean
+        open fun otter(): SpringOtter = SpringOtter(config)
     }
-
-    @Bean
-    open fun getConfig(): OtterConfig = config
-
-    @Bean
-    open fun getSpringOtter(): SpringOtter = SpringOtter(config, enabled)
 }
--- otter-spring-boot-starter/src/main/kotlin/SpringOtter.kt
+++ otter-spring-boot-starter/src/main/kotlin/SpringOtter.kt
@@ -4,23 +4,18 @@ import io.github.goodgoodjm.otter.core.Logger
 import io.github.goodgoodjm.otter.core.Otter
 import io.github.goodgoodjm.otter.core.OtterConfig
 import org.springframework.beans.factory.InitializingBean
-import org.springframework.stereotype.Component
 
 
-@Component
 class SpringOtter(
     otterConfig: OtterConfig,
-    private val enabled: Boolean,
 ) : InitializingBean {
     companion object : Logger
 
     private val otter: Otter = Otter.from(otterConfig)
 
     override fun afterPropertiesSet() {
-        if (!enabled) {
-            logger.info("Otter is not enabled, Stop to load migrations.")
-            return
-        }
+        logger.info("Otter migration start.")
         otter.up()
+        logger.info("Otter migration end.")
     }
 }
